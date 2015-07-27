f8a649e309ea5c04d094e36219de05449325baf7
Jukka Suomela <jukka.suomela@iki.fi>
2015-07-28 01:56:21 +0300

    Can parse sample keys.

--- kutil.py
+++ kutil.py
@@ -1,5 +1,6 @@
 """Miscellaneous utility functions."""
 
+import unittest
 import glob
 import re
 import sys
@@ -35,13 +36,39 @@ def try_search(re, w, i):
         return re.search(w, i)
 
 
+def is_exact(m, w):
+    return m is not None and m.start() == 0 and m.end() == len(w)
+
+
 def exact_match(re, w):
     m = re.match(w)
-    return m is not None and m.start() == 0 and m.end() == len(w)
+    return is_exact(m, w)
 
 
 def try_exact_match(re, w):
     return re is not None and exact_match(re, w)
+
+
+def try_capture(re, w):
+    m = re.match(w)
+    if not is_exact(m, w):
+        return None
+    r = []
+    if m.lastindex is not None:
+        for i in range(m.lastindex):
+            g = m.group(i + 1)
+            if g is not None and g != '':
+                r.append(g)
+    if len(r) == 0:
+        r.append(m.group())
+    return tuple(r)
+
+
+def try_capture_join(re, w, sep=' '):
+    r = try_capture(re, w)
+    if r is None:
+        return None
+    return sep.join(r)
 
 
 class Counter:
@@ -55,3 +82,73 @@ class Counter:
 
     def types(self):
         return len(self.distinct)
+
+
+#### Unit tests
+
+
+class TestCapture(unittest.TestCase):
+    def test_noparen(self):
+        r = re.compile(r'ab*c')
+        self.assertEqual(try_capture(r, ' abc'), None)
+        self.assertEqual(try_capture(r, 'abc '), None)
+        self.assertEqual(try_capture(r, 'abc'), ('abc',))
+        self.assertEqual(try_capture(r, 'abbc'), ('abbc',))
+
+    def test_paren(self):
+        r1 = re.compile(r'a(b*)c')
+        r2 = re.compile(r'a(b+)?c')
+        for r in r1, r2:
+            self.assertEqual(try_capture(r, ' abc'), None)
+            self.assertEqual(try_capture(r, 'abc '), None)
+            self.assertEqual(try_capture(r, 'ac'), ('ac',))
+            self.assertEqual(try_capture(r, 'abc'), ('b',))
+            self.assertEqual(try_capture(r, 'abbc'), ('bb',))
+
+    def test_paren2(self):
+        r1 = re.compile(r'a(b+)?c(d+)?e')
+        r2 = re.compile(r'a(b*)c(d*)e')
+        for r in r1, r2:
+            self.assertEqual(try_capture(r, ' abcde'), None)
+            self.assertEqual(try_capture(r, 'abcde '), None)
+            self.assertEqual(try_capture(r, 'ace'), ('ace',))
+            self.assertEqual(try_capture(r, 'abce'), ('b',))
+            self.assertEqual(try_capture(r, 'acde'), ('d',))
+            self.assertEqual(try_capture(r, 'abcde'), ('b', 'd'))
+            self.assertEqual(try_capture(r, 'abbcdde'), ('bb', 'dd'))
+
+
+class TestCaptureJoin(unittest.TestCase):
+    def test_noparen(self):
+        r = re.compile(r'ab*c')
+        self.assertEqual(try_capture_join(r, ' abc'), None)
+        self.assertEqual(try_capture_join(r, 'abc '), None)
+        self.assertEqual(try_capture_join(r, 'abc'), 'abc')
+        self.assertEqual(try_capture_join(r, 'abbc'), 'abbc')
+
+    def test_paren(self):
+        r1 = re.compile(r'a(b*)c')
+        r2 = re.compile(r'a(b+)?c')
+        for r in r1, r2:
+            self.assertEqual(try_capture_join(r, ' abc'), None)
+            self.assertEqual(try_capture_join(r, 'abc '), None)
+            self.assertEqual(try_capture_join(r, 'ac'), 'ac')
+            self.assertEqual(try_capture_join(r, 'abc'), 'b')
+            self.assertEqual(try_capture_join(r, 'abbc'), 'bb')
+
+    def test_paren2(self):
+        r1 = re.compile(r'a(b+)?c(d+)?e')
+        r2 = re.compile(r'a(b*)c(d*)e')
+        for r in r1, r2:
+            self.assertEqual(try_capture_join(r, ' abcde'), None)
+            self.assertEqual(try_capture_join(r, 'abcde '), None)
+            self.assertEqual(try_capture_join(r, 'ace'), 'ace')
+            self.assertEqual(try_capture_join(r, 'abce'), 'b')
+            self.assertEqual(try_capture_join(r, 'acde'), 'd')
+            self.assertEqual(try_capture_join(r, 'abcde'), 'b d')
+            self.assertEqual(try_capture_join(r, 'abbcdde'), 'bb dd')
+            self.assertEqual(try_capture_join(r, 'abbcdde', 'x'), 'bbxdd')
+
+
+if __name__ == '__main__':
+    unittest.main()
